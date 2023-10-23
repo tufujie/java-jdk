e58d873dd105c40cfc1bd6007ce17c9e4546f61a
jef.tu@foxmail.com <jef.tu@foxmail.com>
2023-10-23 22:05:40 +0800

    分布式锁防止超卖

--- src/main/java/com/jef/redis/RedisJavaUtil.java
+++ src/main/java/com/jef/redis/RedisJavaUtil.java
@@ -2,7 +2,13 @@ package com.jef.redis;
 
 import com.jef.util.PrintUtil;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.params.SetParams;
+
+import java.util.Collections;
 
 /**
  * @author Jef
@@ -27,4 +33,49 @@ public class RedisJavaUtil {
         PrintUtil.printSplitLine();
         return jedis;
     }
+
+    private static final String LOCK_SUCCESS = "OK";
+    private static final Long RELEASE_SUCCESS = 1L;
+
+    /**
+     * 尝试获取分布式锁
+     *
+     * @param jedis                Redis客户端
+     * @param lockKey              锁
+     * @param requestId            请求标识
+     * @param millisecondsToExpire 超期时间
+     * @return 是否获取成功
+     */
+    public static boolean tryGetDistributedLock(Jedis jedis, String lockKey, String requestId, long millisecondsToExpire) {
+        String result = jedis.set(lockKey, requestId, SetParams.setParams().nx().px(millisecondsToExpire));
+        return LOCK_SUCCESS.equals(result);
+    }
+
+    /**
+     * 释放分布式锁
+     *
+     * @param jedis     Redis客户端
+     * @param lockKey   锁
+     * @param requestId 请求标识
+     * @return 是否释放成功
+     */
+    public static boolean releaseDistributedLock(Jedis jedis, String lockKey, String requestId) {
+        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+        Object result = jedis.eval(script, Collections.singletonList(lockKey), Collections.singletonList(requestId));
+        return RELEASE_SUCCESS.equals(result);
+    }
+
+    /**
+     * 获取RedissonClient
+     *
+     * @return RedissonClient
+     */
+    public static RedissonClient getRedissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://localhost:6379").setPassword("root");
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+
 }
