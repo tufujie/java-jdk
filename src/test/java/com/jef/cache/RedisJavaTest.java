311576f60e5d92a6c0e7e8b0c570c7da3191f954
tufujie <tufj@hua-cloud.com.cn>
2023-11-14 15:42:56 +0800

    Redis完善

--- src/test/java/com/jef/cache/RedisJavaTest.java
+++ src/test/java/com/jef/cache/RedisJavaTest.java
@@ -4,13 +4,13 @@ import com.jef.constant.BasicConstant;
 import com.jef.redis.RedisJavaUtil;
 import com.jef.util.PrintUtil;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Tuple;
 import redis.clients.jedis.params.SetParams;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -19,14 +19,36 @@ import java.util.Set;
  */
 public class RedisJavaTest {
 
+    private Jedis jedis;
+
+    @BeforeEach
+    void init() {
+        jedis = RedisJavaUtil.getAuthJedis();
+    }
+
     /**
      * Redis Java String(字符串) 实例
      */
     @Test
     public void testSetAndGetString() {
-        Jedis jedis = RedisJavaUtil.getAuthJedis();
+        jedis = RedisJavaUtil.getAuthJedis();
         // 设置 redis 字符串数据
         jedis.set(BasicConstant.USER_NAME_KEY, BasicConstant.USER_NAME);
+        // 会覆盖
+        jedis.set(BasicConstant.USER_NAME_KEY, BasicConstant.USER_NAME_ZHANGSAN);
+        // 获取存储的数据并输出
+        System.out.println("redis 存储的字符串为: " + jedis.get(BasicConstant.USER_NAME_KEY));
+    }
+
+    @DisplayName("setnx")
+    @Test
+    public void testSetNx() {
+        // 为1，表示无对应key设置成功
+        Long setnxResult = jedis.setnx(BasicConstant.USER_NAME_KEY, BasicConstant.USER_NAME);
+        System.out.println("setnxResult = " + setnxResult);
+        // 不会覆盖，为0，表示有对应key没有设置成功
+        setnxResult = jedis.setnx(BasicConstant.USER_NAME_KEY, BasicConstant.USER_NAME_ZHANGSAN);
+        System.out.println("setnxResult = " + setnxResult);
         // 获取存储的数据并输出
         System.out.println("redis 存储的字符串为: " + jedis.get(BasicConstant.USER_NAME_KEY));
     }
@@ -36,15 +58,14 @@ public class RedisJavaTest {
      */
     @Test
     public void testSetAndGetList() {
-        Jedis jedis = RedisJavaUtil.getAuthJedis();
         // 存储数据到列表中
         jedis.lpush("site-list", "Runoob");
         jedis.lpush("site-list", "Google");
         jedis.lpush("site-list", "Taobao");
         // 获取存储的数据并输出
         List<String> list = jedis.lrange("site-list", 0, 2);
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println("列表项为: " + list.get(i));
+        for (String s : list) {
+            System.out.println("列表项为: " + s);
         }
     }
 
@@ -53,7 +74,6 @@ public class RedisJavaTest {
      */
     @Test
     public void testMessageQueue() {
-        Jedis jedis = RedisJavaUtil.getAuthJedis();
         String key = "site-list";
         // 存储数据到列表中
         jedis.rpush(key, "Runoob");
@@ -70,7 +90,6 @@ public class RedisJavaTest {
      */
     @Test
     public void testGetSetAndGetSet() {
-        Jedis jedis = RedisJavaUtil.getAuthJedis();
         jedis.sadd("site-set", "Runoob");
         jedis.sadd("site-set", "Google");
         jedis.sadd("site-set", "Taobao");
@@ -84,10 +103,9 @@ public class RedisJavaTest {
      */
     @Test
     public void testGetSetAndGeHash() {
-        Jedis jedis = RedisJavaUtil.getAuthJedis();
         jedis.hset(BasicConstant.LOGIN_OBJECT_KEY, BasicConstant.USER_NAME_KEY, BasicConstant.USER_NAME);
         String userName = jedis.hget(BasicConstant.LOGIN_OBJECT_KEY, BasicConstant.USER_NAME_KEY);
-        Assertions.assertTrue(BasicConstant.USER_NAME.equals(userName));
+        Assertions.assertEquals(BasicConstant.USER_NAME, userName);
     }
 
     /**
@@ -95,30 +113,32 @@ public class RedisJavaTest {
      */
     @Test
     public void testGetSetAndGeZSet() {
-        Jedis jedis = RedisJavaUtil.getAuthJedis();
-        jedis.zadd(BasicConstant.PROGRAMMING_LANGUAGE_RANK, 10, "Java");
-        jedis.zadd(BasicConstant.PROGRAMMING_LANGUAGE_RANK, 9, "Python");
-        jedis.zadd(BasicConstant.PROGRAMMING_LANGUAGE_RANK, 8, "GO");
-        jedis.zadd(BasicConstant.PROGRAMMING_LANGUAGE_RANK, 7, "C++");
-        long rankSetSize = jedis.zcard(BasicConstant.PROGRAMMING_LANGUAGE_RANK);
+        String key = BasicConstant.PROGRAMMING_LANGUAGE_RANK;
+        jedis.zadd(key, 10, "Java");
+        jedis.zadd(key, 8, "GO");
+        jedis.zadd(key, 9, "Python");
+        jedis.zadd(key, 7, "C++");
+        long rankSetSize = jedis.zcard(key);
         System.out.println("rankSetSize=" + rankSetSize);
         PrintUtil.printSplitLine();
 
-        Set<Tuple> rankRevSet = jedis.zrevrangeWithScores(BasicConstant.PROGRAMMING_LANGUAGE_RANK, 0L, 2L);
+        Set<Tuple> rankRevSet = jedis.zrevrangeWithScores(key, 0L, 2L);
         System.out.println("正排行=" + rankRevSet);
-        Set<Tuple> rankSet = jedis.zrangeWithScores(BasicConstant.PROGRAMMING_LANGUAGE_RANK, 0L, 2L);
+        Set<String> rankRevSetV2 = jedis.zrevrange(key, 0L, 2L);
+        System.out.println("正排行V2=" + rankRevSetV2);
+        Set<Tuple> rankSet = jedis.zrangeWithScores(key, 0L, 2L);
         System.out.println("逆排行=" + rankSet);
         // 其中有序集成员按分数值递增(从小到大)顺序排列。也就是说数值越大，返回的索引越大
-        long javaRankIndex = jedis.zrank(BasicConstant.PROGRAMMING_LANGUAGE_RANK, "Java");
+        long javaRankIndex = jedis.zrank(key, "Java");
         long javaRank = rankSetSize - javaRankIndex;
         System.out.println("一开始的Java排行=" + javaRank);
         PrintUtil.printSplitLine();
 
         System.out.println("Python分数加2");
-        jedis.zincrby(BasicConstant.PROGRAMMING_LANGUAGE_RANK, 2, "Python");
-        rankRevSet = jedis.zrevrangeWithScores(BasicConstant.PROGRAMMING_LANGUAGE_RANK, 0L, 2L);
+        jedis.zincrby(key, 2, "Python");
+        rankRevSet = jedis.zrevrangeWithScores(key, 0L, 2L);
         System.out.println("正排行=" + rankRevSet);
-        javaRankIndex = jedis.zrank(BasicConstant.PROGRAMMING_LANGUAGE_RANK, "Java");
+        javaRankIndex = jedis.zrank(key, "Java");
         javaRank = rankSetSize - javaRankIndex;
         System.out.println("然后的Java排行=" + javaRank);
     }
@@ -129,12 +149,9 @@ public class RedisJavaTest {
      */
     @Test
     public void testGetAllKeys() {
-        Jedis jedis = RedisJavaUtil.getAuthJedis();
         // 获取Jedis中的所有key数据并输出
         Set<String> keys = jedis.keys("*");
-        Iterator<String> it = keys.iterator();
-        while (it.hasNext()) {
-            String key = it.next();
+        for (String key : keys) {
             System.out.println(key);
         }
     }
@@ -145,7 +162,6 @@ public class RedisJavaTest {
     @Test
     public void testIncr() {
         String key = "incrKey";
-        Jedis jedis = RedisJavaUtil.getAuthJedis();
         long num = jedis.incr(key);
         System.out.println("incrKey=" + num);
         num = jedis.incr(key);
@@ -158,7 +174,6 @@ public class RedisJavaTest {
     @Test
     public void testDecr() {
         String key = "decrKey";
-        Jedis jedis = RedisJavaUtil.getAuthJedis();
         jedis.set(key, "10");
         long num = jedis.decr(key);
         System.out.println("decrKey=" + num);
@@ -171,7 +186,6 @@ public class RedisJavaTest {
      */
     @Test
     public void testDel() {
-        Jedis jedis = RedisJavaUtil.getAuthJedis();
         jedis.hset(BasicConstant.LOGIN_OBJECT_KEY, BasicConstant.USER_NAME_KEY, BasicConstant.USER_NAME);
         String userName = jedis.hget(BasicConstant.LOGIN_OBJECT_KEY, BasicConstant.USER_NAME_KEY);
         System.out.println("userName=" + userName);
@@ -208,10 +222,21 @@ public class RedisJavaTest {
 
     @DisplayName("设置过期时间并取出")
     @Test
-    void testSetTTLAndGet() {
-        Jedis jedis = RedisJavaUtil.getAuthJedis();
+    void testSetTTLAndGet() throws InterruptedException {
         jedis.set(BasicConstant.USER_NAME_KEY, BasicConstant.USER_NAME, SetParams.setParams().ex(60));
-        jedis.get(BasicConstant.USER_NAME_KEY);
+        Long ttl = jedis.ttl(BasicConstant.USER_NAME_KEY);
+        System.out.println("ttl=" + ttl);
+        Thread.sleep(1000);
+        ttl = jedis.ttl(BasicConstant.USER_NAME_KEY);
+        System.out.println("ttl=" + ttl);
     }
+
+    @DisplayName("其他功能")
+    @Test
+    void testOtherFunction() {
+        jedis.save();
+        jedis.bgsave();
+    }
+
 
 }
